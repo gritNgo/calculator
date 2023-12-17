1afe9159f4f3920434e78d30fc7e53d56c0c236a
gritNgo <dustspeck00@gmail.com>
2023-12-17 15:03:56 +0000

    operate() fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Calculator() {
+function calculator() {
     this.operations = {
         "-": (a, b) => a - b,
         "+": (a, b) => a + b,
@@ -11,7 +11,32 @@ function Calculator() {
         a = +split[0],
         op = split[1],
         b = +split[2]
-    }
     
-    console.log(add(3, 9))
+        if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+            return NaN;
+        }
+
+        return this.methods[op](a, b);
+    }  
 }
+
+const display = document.querySelector(".display");
+const buttons = [...document.querySelectorAll("operands-btn")];
+// const values = buttons.map(button => button.textContent)
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        
+    })
+})
+
+
+// document.querySelectorAll('.operands-btn').forEach(el =>{
+//     el.addEventListener('click', () =>{
+//       digit_pressed(el.textContent);
+//     });
+//   });
+//   function digit_pressed(digit) {
+//     console.log(digit);
+//   }
+
